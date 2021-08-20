a66149df8ca59046aa3bf1f4392d2b88b3bf33a8
Kasun-se2018-023 <73273550+Kasun-se-2018-023@users.noreply.github.com>
2021-08-20 13:34:42 +0530

    add kernel files

--- kmain.c
+++ kmain.c
@@ -1,25 +1,32 @@
-#include "drivers/frame_buffer/frame_buffer.h"
-#include "drivers/serial_port/serial_port.h"
-#include "Segmentation/gdt.h"
+#include "gdt.h"
+#include "idt.h"
+#include "logger.h"
+#include "serial_port.h"
+#include "timer.h"
+#include "types.h"
 
-
-
-
-
-
+/* Function to initialize */
 void init() {
- 
+  /* Initialize segment descriptor tables */
   init_gdt();
 
-  
+  /* Initialize segment descriptor tables */
+  init_idt();
+
+  /* Initialize timer interrupt */
+  init_timer(TIMER_FREQUENCY);
+
+  /* Initialize serial port */
   serial_configure(SERIAL_COM1_BASE, Baud_115200);
 }
 
-
-int kmain() {
+/* Kernel Main */
+s32int kmain() {
   init();
-  char buffer[20] = "Welcome to kmOS\n";
-  fb_write(buffer, 20);
-  serial_write(SERIAL_COM1_BASE, buffer, 20);
+  s8int buffer[20] = "This works!!!!!!!\n";
+  print_screen(buffer, 20);
+  print_serial(buffer, 20);
+  asm volatile("int $0x3");
+  asm volatile("int $0x22");
   return 0;
 }
